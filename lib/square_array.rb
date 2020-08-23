df8b7f55fb1cf2167ee76d60e31a99b7a7ac5b2e
Jonathan Xiao <jonathanx111@gmail.com>
2020-08-23 21:56:06 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
-  # your code here
+  count = 0 
+  new = []
+  while array[count] do
+    new << (array[count] ** 2)
+    count += 1 
+  end
+  
+  new
 end
+
+# def square_array(array)
+#   new = []
+#   array.length.times{ |index| new << (array[index] ** 2) }
+#   new
+# end
+
+# print square_array([1,2,3,4,5])
